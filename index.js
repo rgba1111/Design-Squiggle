4d0c703ab0d85a82e8faea50317be88f1ecc7e06
rgba1111 <hello@moritzkuhn.com>
2023-07-16 19:36:42 +0200

    Added Lottie files, interactions and assets

--- index.js
+++ index.js
@@ -17,7 +17,76 @@ function copy() {
 }
 
 function resetButton() {
-  elem.innerHTML = "E-Mail";
+  elem.innerHTML = "Email";
   elem.classList.remove("btn-active");
   wait = false;
 }
+
+// sound set-up
+  const pause_sound = new Audio("../../content/3_about/audio/pause.wav");
+  const play_sound = new Audio("../../content/3_about/audio/play.wav");
+
+
+// lottie set-up
+
+var params = {
+  container: document.getElementById("lottie-container"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+};
+
+var anim;
+anim = lottie.loadAnimation(params);
+
+const player = document.querySelector("#lottie-container");
+
+const body = document.querySelector("body");
+const message_content = document.querySelector("#message-content");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+// lottie interactivity 
+
+LottieInteractivity.create({
+  player: "#lottie-container",
+  mode: "chain",
+  autoplay: true,
+  actions: [
+    {
+      state: "loop",
+      transition: "hover",
+      frames: [0, 243],
+      reset: true,
+    },
+    {
+      state: "none",
+      position: {
+        x: [0, 1],
+        y: [-1, 2],
+      },
+      transition: "seek",
+      frames: [0, 243],
+      reset: true,
+    },
+  ],
+});
+
+
+// theme switch
+
+player.addEventListener("mouseenter", function () {
+  body.style.backgroundColor = "var(--black)";
+  message_content.style.color = "var(--offwhite)";
+  footer.style.opacity = "0";
+  header.style.opacity = "0";
+});
+
+player.addEventListener("mouseleave", function () {
+  body.style.backgroundColor = "var(--white)";
+  message_content.style.color = "var(--black)";
+  footer.style.opacity = "1";
+  header.style.opacity = "1";
+  player.setDirection(1);
+  player.play();
+});
